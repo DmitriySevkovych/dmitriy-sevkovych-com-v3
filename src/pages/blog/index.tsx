288e4f9c98265f9f96c95b4b88ae749544a90802
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2024-08-22 00:18:50 +0200

    feat: adjust blog index page content and style

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -29,53 +29,50 @@ const BlogPage: React.FC<BlogPageProps> = ({ posts }) => {
                     <span>- Leslie Lamport</span>
                 </blockquote>
 
-                <p>
-                    This part of my website is my digital notebook. Why exactly
-                    does it exist and what is it about? I guess this is my arena
-                    of self-reflection, and a way to persist some impressions,
-                    ideas, solutions to certain problems or simply memories that
-                    seem worth persisting to me. My writing is mostly for
-                    myself. Maybe my kids will read it one day, or maybe a
-                    stranger will stumble across it and find some of it
-                    insightful. I would be happy if it helps someone, but if it
-                    doesn&apos;t, that&apos;s fine too.
-                </p>
+                <div className="flex w-full max-w-6xl flex-col gap-5 self-center">
+                    <p className="leading-relaxed">
+                        This is my digital notebook. Why exactly does it exist
+                        and what is it about? I guess this is both an arena of
+                        self-reflection and a way to persist what seems worth
+                        persisting to me. My writing is mostly for myself. Maybe
+                        my kids will read it one day, or maybe an unknown
+                        stranger will find it insightful. I would be happy if my
+                        writing helps someone, but if it doesn&apos;t,
+                        that&apos;s fine too.
+                    </p>
 
-                <p>
-                    I have decided to try to make a habit of writing a few lines
-                    on any topic that I find interesting at the moment. I once
-                    read the quote that you can find at the top of this page and
-                    it stung. I dislike writing, especially clackering on a
-                    keyboard, even though I do it a lot. But the urge to collect
-                    my thoughts and put them in order seems to be winning the
-                    upper hand. Or maybe I am just getting older😅.
-                </p>
+                    {/* <p className='leading-relaxed'>
+                        I have decided to make a habit of writing a few lines
+                        on any topic that I find interesting at the moment. I once
+                        read the quote that you can find at the top of this page and
+                        it stung. I dislike writing, especially clackering on a
+                        keyboard, even though I do it a lot. But the urge to collect
+                        my thoughts and put them in order seems to be winning the
+                        upper hand. Or maybe I am just getting older😅.
+                    </p> */}
 
-                <p>
-                    The topics in this notebook could range from notes on my
-                    professional occupation (which would be software engineering
-                    and machine learning) or my humble experiments with being an
-                    entrepreneur, to some lessons learned from investing, to
-                    books that I read, childhood memories, or, should I happen
-                    to ever find myself at the pinnacle of my philosophic
-                    ability, my thoughts on (my) life as a whole. I probably
-                    won&apos;t stick to a certain format, maybe not even to only
-                    one language. Just go with the flow. My target audience in
-                    this case is actually just me, so the main authority on what
-                    to write and how to write it will be my mood.
-                </p>
+                    <p className="leading-relaxed">
+                        The topics in this notebook will range from notes on my
+                        professional occupation (which would be software
+                        engineering and machine learning) or my experiments with
+                        being an entrepreneur, to lessons learned from
+                        investing, to thoughts on being a parent, to childhood
+                        memories. I don&apos;t stick to a certain format, topic,
+                        nor even to only one language.
+                    </p>
 
-                <p>
-                    In case you&apos;ve come this far, have fun browsing through
-                    my latest posts! Or if you want to start at the beginning,
-                    check out{' '}
-                    <Link
-                        href="/blog/2024-07-06-about_me"
-                        className="text-accent"
-                    >
-                        my background story as I perceive it.
-                    </Link>
-                </p>
+                    <p className="leading-relaxed">
+                        And since you are reading this: just have fun browsing
+                        through my latest posts! Or if you want to start at the
+                        beginning, check out{' '}
+                        <Link
+                            href="/blog/2024-07-06-about_me"
+                            className="text-accent"
+                        >
+                            my background story as I perceive it.
+                        </Link>
+                    </p>
+                </div>
 
                 <div className="mt-3 grid grid-cols-1 gap-5 lg:grid-cols-2">
                     {posts.map((post) => (
