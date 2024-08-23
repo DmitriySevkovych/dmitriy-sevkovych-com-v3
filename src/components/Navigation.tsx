b4e186ba36e423d3efbc307d47ba10ffc7a92d84
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2024-08-23 15:55:21 +0200

    refactor: extract navigation items to dedicated component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,27 +1,37 @@
+import { cn } from '@/lib/utils'
 import Link from 'next/link'
+import React from 'react'
 
-const Navigation = () => {
+interface NavigationItemProps extends React.HTMLProps<HTMLAnchorElement> {}
+
+const NavigationItem: React.FC<NavigationItemProps> = ({
+    className,
+    children,
+    href,
+}) => {
     return (
-        <div className="mb-8 flex w-full items-baseline gap-4 lg:gap-6">
-            <Link
-                href="/"
-                className="text-xl text-foreground transition-colors hover:text-accent"
-            >
+        <Link
+            href={href as string}
+            className={cn(
+                'text-foreground transition-colors hover:text-accent',
+                className
+            )}
+        >
+            {children}
+        </Link>
+    )
+}
+
+const Navigation: React.FC = () => {
+    return (
+        <nav className="mb-8 flex w-full items-baseline gap-4 lg:gap-6">
+            <NavigationItem href="/" className="text-xl">
+                {' '}
                 Dmitriy&apos;s Website
-            </Link>
-            <Link
-                href="/blog"
-                className="text-foreground transition-colors hover:text-accent"
-            >
-                Blog
-            </Link>
-            <Link
-                href="/cv"
-                className="text-foreground transition-colors hover:text-accent"
-            >
-                CV
-            </Link>
-        </div>
+            </NavigationItem>
+            <NavigationItem href="/blog">Blog</NavigationItem>
+            <NavigationItem href="/cv">CV</NavigationItem>
+        </nav>
     )
 }
 
