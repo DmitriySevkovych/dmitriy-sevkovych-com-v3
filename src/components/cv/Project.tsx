7dc5859d055954772ec0bb70aa6493a876ec9c67
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2024-08-30 11:26:01 +0200

    feat: add tech stack carousel to cv page

--- src/components/cv/Project.tsx
+++ src/components/cv/Project.tsx
@@ -1,5 +1,7 @@
 import { CVProjectItem } from '@/model/cv'
+import Autoplay from 'embla-carousel-autoplay'
 import { ChevronRight } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 import {
@@ -10,8 +12,53 @@ import {
     CardHeader,
     CardTitle,
 } from '../ui/card'
+import { Carousel, CarouselContent, CarouselItem } from '../ui/carousel'
 
 type ProjectProps = CVProjectItem
+
+const TechStack: React.FC<Pick<ProjectProps, 'techstack'>> = ({
+    techstack,
+}) => {
+    if (!techstack) return null
+
+    return (
+        <div className="mt-4 w-full">
+            <p className="mb-5 font-medium">Tech Stack</p>
+            <Carousel
+                opts={{
+                    // align: "start",
+                    loop: true,
+                }}
+                plugins={[
+                    Autoplay({
+                        delay: 2000,
+                    }),
+                ]}
+            >
+                <CarouselContent>
+                    {techstack.map((tech, i) => (
+                        <CarouselItem key={i} className="basis-1/3">
+                            <div className="flex h-full flex-col items-center justify-between gap-1">
+                                <div className="m-auto">
+                                    <Image
+                                        width={32}
+                                        height={32}
+                                        src={`/icons/${tech.image}.svg`}
+                                        alt={tech.caption}
+                                    />
+                                </div>
+                                <div>{tech.caption}</div>
+                            </div>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+            </Carousel>
+            {/* <div className='pt-1 pl-2'>
+
+            </div> */}
+        </div>
+    )
+}
 
 const Responsibilities: React.FC<Pick<ProjectProps, 'responsibilities'>> = ({
     responsibilities,
@@ -44,18 +91,20 @@ const Project: React.FC<ProjectProps> = ({
     techstack,
 }) => {
     return (
-        <Card>
+        <Card className="flex flex-col">
             <CardHeader>
                 <CardDescription>{`${date_from} - ${date_until} // ${location}`}</CardDescription>
                 <CardTitle className="text-xl">
                     {title} <br /> {`@ ${client}`}
                 </CardTitle>
             </CardHeader>
-            <CardContent>
+            <CardContent className="flex-grow">
                 <p>{description}</p>
                 <Responsibilities responsibilities={responsibilities} />
             </CardContent>
-            <CardFooter></CardFooter>
+            <CardFooter>
+                <TechStack techstack={techstack} />
+            </CardFooter>
         </Card>
     )
 }
